204572f78c280910354f1582df4b7a2cae9b1bdc
Kundan <kundank191@gmail.com>
2018-05-31 13:53:42 +0530

    feat : Deatail activity now populates UI with data

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,37 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView alsoKnownAsTV, placeOfOriginTV, descriptionTV, ingredientsTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Toolbar toolbar = findViewById(R.id.app_bar_detailed);
+        setSupportActionBar(toolbar);
+
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        alsoKnownAsTV = findViewById(R.id.also_known_tv);
+        placeOfOriginTV = findViewById(R.id.origin_tv);
+        descriptionTV = findViewById(R.id.description_tv);
+        ingredientsTV = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +69,20 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        alsoKnownAsTV.setText(getFormattedList(sandwich.getAlsoKnownAs()));
+        placeOfOriginTV.setText(sandwich.getPlaceOfOrigin());
+        descriptionTV.setText(sandwich.getDescription());
+        ingredientsTV.setText(getFormattedList(sandwich.getIngredients()));
+    }
 
+    /**
+     *
+     * @param list Takes in a list of string and returns a formatted form of that list removing brackets
+     * @return a string
+     */
+    private String getFormattedList(List<String> list){
+        String string = list.toString();
+        return string.substring(1,string.length() - 1);
     }
 }
